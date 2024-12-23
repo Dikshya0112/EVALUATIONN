ec0a39a850427ec58ae0ab27a4adbb07d2f1b1fb
Sudikshya Patra <sudikshyapatra0112@gmail.com>
2024-12-23 14:00:32 +0530

    .

---
+++ Q.28/react/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+    return (
+        <div>
+            <Greeting name="Sir" />
+        </div>
+    );
+}
+
+export default App;
